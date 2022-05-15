f5b46a47960e93b51fb59ecbc3088e102a3a4d6d
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-05-15 13:55:20 +0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Services from "./Pages/Services";
 
 
 function App() {
   return (
     <div>
-      
+      <Navbar> 
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/services" element={<Services></Services>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
